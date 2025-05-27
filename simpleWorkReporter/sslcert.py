a2b23ea6258becd3e5fe865c43158017649b75cb
na-parse <nathan@unit03.net>
2025-05-26 18:37:41 -0700

    ssl cert built-in using cryptography module

---
+++ simpleWorkReporter/sslcert.py
@@ -0,0 +1,56 @@
+'''
+simpleWorkReporter - sslcert.py
+    Provides a basic method for creating self-signed certificates for running
+    the web server with SSL.
+
+    This produces results equivalent to the openssl command:
+        openssl req -x509 -newkey rsa:4096 -nodes \
+            -out cert.pem -keyout key.pem \
+            -days 2920
+'''
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+import datetime
+from pathlib import Path
+
+
+def create_ssl_files(cert_path: Path, key_path: Path):
+    # Generate private key
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=4096,
+    )
+
+    # Create certificate
+    subject = issuer = x509.Name([
+        x509.NameAttribute(NameOID.COMMON_NAME, u"localhost"),
+        x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"simpleWorkReporter"),
+        x509.NameAttribute(NameOID.ORGANIZATIONAL_UNIT_NAME, u"SelfSignForFlask"),
+    ])
+
+    cert = x509.CertificateBuilder().subject_name(
+            subject
+        ).issuer_name(
+            issuer
+        ).public_key(
+            private_key.public_key()
+        ).serial_number(
+            x509.random_serial_number()
+        ).not_valid_before(
+            datetime.datetime.utcnow()
+        ).not_valid_after(
+            datetime.datetime.utcnow() + datetime.timedelta(days=2920)
+        ).sign(private_key, hashes.SHA256())
+
+    # Write files
+    with open(cert_path, "wb") as f:
+        f.write(cert.public_bytes(serialization.Encoding.PEM))
+
+    with open(key_path, "wb") as f:
+        f.write(private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        ))
